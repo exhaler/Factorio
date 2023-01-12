c9819050ccd01b608bf0df87ea124beddca57869
Fadi Barakat <fadibar@gmail.com>
2023-01-11 23:01:33 -0500

    removed locomotive speed

--- Personal_Space_Exploration_Modpack/data-updates.lua
+++ Personal_Space_Exploration_Modpack/data-updates.lua
@@ -1,7 +1,8 @@
-if data.raw["locomotive"]["space-locomotive"] then
-   data.raw["locomotive"]["space-locomotive"].max_speed = 40;
-   data.raw["locomotive"]["space-locomotive"].braking_force = 750;
-end
+-- if data.raw["locomotive"]["space-locomotive"] then
+--    data.raw["locomotive"]["space-locomotive"].max_speed = 40;
+--    data.raw["locomotive"]["space-locomotive"].max_power = "31MW";
+--    data.raw["locomotive"]["space-locomotive"].braking_force = 750;
+-- end
 
 if data.raw["fluid-wagon"]["space-fluid-wagon"] then
    data.raw["fluid-wagon"]["space-fluid-wagon"].capacity = 75000;
