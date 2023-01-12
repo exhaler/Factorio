a605d45f2cff1693b3f18f933466b963b123e0f1
Fadi Barakat <fadibar@gmail.com>
2023-01-11 22:48:01 -0500

    Changed space train speed

--- Personal_Space_Exploration_Modpack/data-updates.lua
+++ Personal_Space_Exploration_Modpack/data-updates.lua
@@ -1,3 +1,12 @@
+if data.raw["locomotive"]["space-locomotive"] then
+   data.raw["locomotive"]["space-locomotive"].max_speed = 40;
+   data.raw["locomotive"]["space-locomotive"].braking_force = 750;
+end
+
 if data.raw["fluid-wagon"]["space-fluid-wagon"] then
-   data.raw["fluid-wagon"]["space-fluid-wagon"].capacity = 75000
+   data.raw["fluid-wagon"]["space-fluid-wagon"].capacity = 75000;
+end
+
+if data.raw["cargo-wagon"]["space-cargo-wagon"] then
+   data.raw["cargo-wagon"]["space-cargo-wagon"].inventory_size = 100;
 end
