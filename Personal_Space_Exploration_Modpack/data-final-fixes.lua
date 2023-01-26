4a68b33d1f041612a549a078b20cad5af703547d
Fadi Barakat <fadibar@gmail.com>
2023-01-26 14:54:49 -0500

    More enhancements

---
+++ Personal_Space_Exploration_Modpack/data-final-fixes.lua
@@ -0,0 +1,10 @@
+if data.raw["assembling-machine"]["kr-quantum-computer"] then
+   data.raw["assembling-machine"]["kr-quantum-computer"].allowed_effects = { "consumption", "speed", "pollution",
+      "productivity" }
+end
+
+if data.raw["assembling-machine"]["kr-quantum-computer-spaced"] then
+   data.raw["assembling-machine"]["kr-quantum-computer-spaced"].allowed_effects = { "consumption", "speed",
+      "pollution",
+      "productivity" }
+end
--- Personal_Space_Exploration_Modpack/data-updates.lua
+++ Personal_Space_Exploration_Modpack/data-updates.lua
@@ -15,3 +15,29 @@ end
 if data.raw["furnace"]["kr-fluid-burner"] then
    data.raw["furnace"]["kr-fluid-burner"].se_allow_in_space = true
 end
+
+if data.raw["electric-energy-interface"]["kr-wind-turbine"] then
+   -- data.raw["electric-energy-interface"]["kr-wind-turbine"].effectivity = 10
+   data.raw["electric-energy-interface"]["kr-wind-turbine"].energy_production = "3MW"
+   data.raw["electric-energy-interface"]["kr-wind-turbine"].energy_source = {
+      type = "electric",
+      buffer_capacity = "3MJ",
+      usage_priority = "primary-output",
+      output_flow_limit = "3MW",
+      render_no_power_icon = false,
+   }
+end
+
+
+
+-- if data.raw["transport-belt"]["extreme-fast-belt"] then
+--    data.raw["transport-belt"]["extreme-fast-belt"].se_allow_in_space = true
+-- end
+
+-- if data.raw["underground-belt"]["extreme-underground-belt"] then
+--    data.raw["underground-belt"]["extreme-underground-belt"].se_allow_in_space = true
+-- end
+
+-- if data.raw["splitter"]["extreme-fast-splitter"] then
+--    data.raw["splitter"]["extreme-fast-splitter"].se_allow_in_space = true
+-- end
