eac1990525337a13279345bc29f19cbb64aa7696
Fadi Barakat <fadibar@gmail.com>
2023-03-28 20:47:47 -0400

    removed train material capacity

--- Personal_Maglev_Modpack/data-updates.lua
+++ Personal_Maglev_Modpack/data-updates.lua
@@ -1,7 +1,3 @@
 if data.raw["fluid-wagon"]["space-fluid-wagon"] then
    data.raw["fluid-wagon"]["space-fluid-wagon"].capacity = 75000;
 end
-
-if data.raw["cargo-wagon"]["space-cargo-wagon"] then
-   data.raw["cargo-wagon"]["space-cargo-wagon"].inventory_size = 100;
-end
